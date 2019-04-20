2cf8f10716574e7a67e87ecda26949fcb530c2f5
rs-kkit0 <38030070+rs-kkit0@users.noreply.github.com>
2019-04-20 15:18:44 +0900

    一覧画面への画面遷移追加

--- app/src/main/java/com/example/deadlinetimer/MainActivity.kt
+++ app/src/main/java/com/example/deadlinetimer/MainActivity.kt
@@ -1,12 +1,25 @@
 package com.example.deadlinetimer
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun launchListActivity(view: View) {
+        Log.d(LOG_TAG, "Button clicked!")
+        val intent = Intent(this, ListActivity::class.java)
+        startActivity(intent)
+    }
+
+    companion object {
+        private val LOG_TAG: String = MainActivity::class.java.simpleName
     }
 }
